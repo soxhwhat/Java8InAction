086575bb8ee2d0841abec6af76712dd26c0028ca
soxhwhat <1246669436@qq.com>
2023-02-12 21:58:45 +0800

    docs: 匿名内部类以及this指针指向对象的实例域

--- src/main/java/lambdasinaction/chap2/MeaningOfThis.java
+++ src/main/java/lambdasinaction/chap2/MeaningOfThis.java
@@ -2,6 +2,7 @@ package lambdasinaction.chap2;
 
 public class MeaningOfThis
 {
+	// Lambda表达式引用的外部变量必须是final或者事实上的final
 	public final int value = 4;
 	public void doIt()
 	{
@@ -10,6 +11,7 @@ public class MeaningOfThis
 			public final int value = 5;
 			public void run(){
 				int value = 10;
+				// this关键字实际上引用的是匿名内部类的对象
 				System.out.println(this.value);
 			}
 		};
